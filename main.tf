88abcdbf320b0f65a80d2e74e9c5703f0d035bbd
crisgonzalezallaria <143832930+crisgonzalezallaria@users.noreply.github.com>
2024-06-08 12:14:30 -0300

    wip

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 resource "azurerm_resource_group" "this" {
-  name     = "function${var.name}"
+  name     = "Function${var.name}"
   location = "East US"
 }
 
 resource "azurerm_storage_account" "this" {
-  name                     = "function${var.name}"
+  name                     = lower("Function${var.name}")
   resource_group_name      = azurerm_resource_group.this.name
   location                 = azurerm_resource_group.this.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 resource "azurerm_service_plan" "this" {
-  name                = "example-app-service-plan"
+  name                = "Function${var.name}"
   resource_group_name = azurerm_resource_group.this.name
   location            = azurerm_resource_group.this.location
   os_type             = "Linux"
@@ -19,15 +19,15 @@ resource "azurerm_service_plan" "this" {
 }
 
 resource "azurerm_container_registry" "this" {
-  name                = "function${var.name}"
+  name                = lower("Function${var.name}")
   resource_group_name = azurerm_resource_group.this.name
   location            = azurerm_resource_group.this.location
   sku                 = "Basic"
   admin_enabled       = true
 }
 
-resource "azurerm_application_insights" "linux-application-insights" {
-  name                = "application-insights-${var.name}"
+resource "azurerm_application_insights" "this" {
+  name                = "Function${var.name}"
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
   application_type    = "other"
@@ -36,7 +36,7 @@ resource "azurerm_application_insights" "linux-application-insights" {
 
 
 resource "azurerm_linux_function_app" "this" {
-  name                = "nuberu${var.name}"
+  name                = "Function${var.name}"
   resource_group_name = azurerm_resource_group.this.name
   location            = azurerm_resource_group.this.location
 
@@ -44,11 +44,23 @@ resource "azurerm_linux_function_app" "this" {
   storage_account_access_key = azurerm_storage_account.this.primary_access_key
   service_plan_id            = azurerm_service_plan.this.id
 
+  app_settings = {
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE = false # WEBSITES_ENABLE_APP_SERVICE_STORAGE to false, since your app content is provided in the container itself
+  }
+
   site_config {
-    application_insights_key               = azurerm_application_insights.linux-application-insights.instrumentation_key
-    application_insights_connection_string = azurerm_application_insights.linux-application-insights.connection_string
     application_stack {
-      python_version = 3.9 #FUNCTIONS_WORKER_RUNTIME        
+      docker {
+        image_name   = "functhird"
+        image_tag    = "latest"
+        registry_url = azurerm_container_registry.this.id
+        registry_username = azurerm_container_registry.this.admin_username
+        registry_password = azurerm_container_registry.this.admin_password
+      }
     }
+    application_insights_connection_string = azurerm_application_insights.this.connection_string
+    application_insights_key = azurerm_application_insights.this.instrumentation_key
+
+    http2_enabled = true
   }
 }
